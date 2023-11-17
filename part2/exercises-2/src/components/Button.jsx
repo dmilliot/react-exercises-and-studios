7257130822fae60106bde8d2cae383deca2d6753
Derrick Milliot <merlot5390@gmail.com>
2023-11-16 18:13:20 -0600

    exercises done

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -1,9 +1,12 @@
 import './styles.css';
 
 function Button() {
+   function onLearnMore() {
+      alert("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA, SPLASH!");
+   };
 
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
